69462205fe044f9e681a0c10fc60374907cccdaf
mistletoe-sb <sv1026@naver.com>
2023-01-26 22:26:30 +0900

    230126 Lee v0.0.1

--- src/main/java/com/logus/dailystory/dao/IDailystoryDAO.java
+++ src/main/java/com/logus/dailystory/dao/IDailystoryDAO.java
@@ -2,6 +2,8 @@ package com.logus.dailystory.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.logus.dailystory.model.DailystoryVO;
 
 // 일일 스토리 DAO 인터페이스
@@ -12,5 +14,9 @@ public interface IDailystoryDAO {
 	public abstract int deleteDailystory(int dailystoryCode);		// 일일 스토리 삭제
 	public abstract DailystoryVO selectDailystoryInfo(int dailystoryCode);			// 일일 스토리 상세 내용 조회
 	public abstract List<DailystoryVO> selectDailystoryList(String memberNickname);	// 일일 스토리 목록 조회
-	public abstract List<DailystoryVO> findDailystoryList(String option, String search);	// 옵션에 따라 검색
+	public abstract List<DailystoryVO> findDailystoryList(
+			@Param("option") String option, @Param("search") String search);		// 옵션에 따라 검색(전체 검색)
+	public abstract List<DailystoryVO> findDailystoryList(
+			@Param("option") String option, @Param("search") String search, 
+			@Param("memberNickname") String memberNickname);						// 옵션에 따라 검색(서재 내 검색)
 }
