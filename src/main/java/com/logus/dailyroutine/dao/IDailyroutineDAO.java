d4f958f7a0908c9918992f54d5be153639d60f45
mistletoe-sb <sv1026@naver.com>
2023-01-26 23:45:51 +0900

    230126 Lee v0.1.0

--- src/main/java/com/logus/dailyroutine/dao/IDailyroutineDAO.java
+++ src/main/java/com/logus/dailyroutine/dao/IDailyroutineDAO.java
@@ -2,14 +2,19 @@ package com.logus.dailyroutine.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.logus.dailyroutine.model.DailyroutineVO;
 
 public interface IDailyroutineDAO {
 	public void insertDailyroutine(DailyroutineVO vo);
-	public int updateDailyroutine(DailyroutineVO vo);
+	
+	//public int select();	//1번 active 정보를 반환
+	public int updateRoutineActive();	//1번 반환받은 정보 기반으로 
+	public int updateDailyroutine(DailyroutineVO vo);	//2번 active 포함 업데이트
 	public int deleteDailyroutine(int dailyroutineCode);
-	public DailyroutineVO selectDailyroutineInfo(String dailyroutineCode);
-	public List<DailyroutineVO> selectDailyroutineList(String memberId);
-	public List<DailyroutineVO> findDailyroutineList(String search);
+	public DailyroutineVO selectDailyroutineInfo(int dailyroutineCode);
+	public List<DailyroutineVO> selectDailyroutineList(String memberNickname);
+	public List<DailyroutineVO> findDailyroutineList(@Param("option") String option, @Param("search") String search);
 	
 }
--- src/main/java/com/logus/dailyroutine/model/DailyroutineVO.java
+++ src/main/java/com/logus/dailyroutine/model/DailyroutineVO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class DailyroutineVO {
 	private int dailyroutineCode;
-	private String memberId;
+	private String memberNickname;
 	private int dailyroutineWeekopt;
 	private String dailyroutineTitle;
 	private String dailyroutineActive;
