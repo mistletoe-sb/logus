4def7c3d04452400ccec6395ac014f653a025c88
dramMyCoding <noseola24@gmail.com>
2023-01-27 20:46:41 +0900

    230127 Noh v1.5

--- src/main/java/com/logus/controller/BoardController.java
+++ src/main/java/com/logus/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.logus.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -34,4 +35,10 @@ public class BoardController {
 			return "manager/reportdetail";
 		}
 	}
+	@GetMapping(value="/manager/chart")
+	public String getBoardDetail1() {
+			return "manager/chartMain";
+		
+	}
+	
 }
