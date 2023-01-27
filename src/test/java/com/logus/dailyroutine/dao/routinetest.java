254a80949a6883456523a5a486003f00e8e5a6b7
mistletoe-sb <sv1026@naver.com>
2023-01-27 11:45:19 +0900

    230127 Choi v0.1.3 test Lee

--- src/test/java/com/logus/dailyroutine/dao/routinetest.java
+++ src/test/java/com/logus/dailyroutine/dao/routinetest.java
@@ -1,9 +1,8 @@
 package com.logus.dailyroutine.dao;
 
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
 
 public class routinetest {
 
